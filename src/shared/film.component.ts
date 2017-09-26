6610e3432abdfb085e25ee383d2d0e5a51e97c5c
Karim <kabdeselam@synergicpartners.com>
2017-09-26 09:56:08 +0200

    Templates expression

--- src/shared/film.component.ts
+++ src/shared/film.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
     styleUrls: ['./film.component.css']
 })
 export class FilmComponent {
+
+    //declaramos la propiedad/campo film en la clase
+    public data: any = {
+        name: 'Spectre 007',
+        gender: 'Aventura',
+        image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjM2Nzg4MzkwOF5BMl5BanBnXkFtZTgwNzA0OTE3NjE@._V1_UX182_CR0,0,182,268_AL_.jpg',
+        imdbUrl:'http://www.imdb.com/title/tt2379713/'
+    }
 }
