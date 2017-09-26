ebcb29028da97f7fd7054e480ed8fe5b9284d451
Karim <kabdeselam@synergicpartners.com>
2017-09-26 11:20:40 +0200

    *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,14 @@ export class AppComponent {
             gender: 'Aventura',
             image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjM2Nzg4MzkwOF5BMl5BanBnXkFtZTgwNzA0OTE3NjE@._V1_UX182_CR0,0,182,268_AL_.jpg',
             imdbUrl: 'http://www.imdb.com/title/tt2379713/'
-        },{
+        },
+        {
             name: 'Solo en casa',
-            gender: 'Comedia',
+            // gender: 'Comedia',
             image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BNmQzYjEzYTQtNzNhZi00NmEwLThiZDMtMWYyNjRmZWY0ZTdkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg',
             imdbUrl: 'http://www.imdb.com/title/tt0099785/'
-        },{
+        },
+        {
             name: 'Mascotas',
             gender: 'Comedia',
             image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjIzMzA1OTkzNV5BMl5BanBnXkFtZTgwODE3MjM4NzE@._V1_UX182_CR0,0,182,268_AL_.jpg',
