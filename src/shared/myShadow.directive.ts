594cdbbc936435b825dd3836db3b417fc0adc021
Karim <kabdeselam@synergicpartners.com>
2017-09-26 12:51:42 +0200

    HostListener

--- src/shared/myShadow.directive.ts
+++ src/shared/myShadow.directive.ts
@@ -4,12 +4,24 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
     selector:'[my-shadow]'
 })
 export class MyShadowDirective {
+
     el: ElementRef = null
+
     constructor(el: ElementRef) {
         this.el = el;
         this.setShadow('2px 2px 10px #ccc')
     }
+
     setShadow(shadow) {
         this.el.nativeElement.style.boxShadow = shadow;
+    }
+
+    @HostListener('mouseenter')
+    onMouseEnter(){
+        this.setShadow('5px 5px 10px #f00');
+    }
+    @HostListener('mouseleave')
+    onMouseLeave(){
+        this.setShadow('2px 2px 10px #ccc');
     }
 }
