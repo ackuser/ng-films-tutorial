3a474071f6a36c6956b036f81f1da9892946865f
Karim <kabdeselam@synergicpartners.com>
2017-09-26 10:00:38 +0200

    Eventos

--- src/shared/film.component.ts
+++ src/shared/film.component.ts
@@ -13,4 +13,9 @@ export class FilmComponent {
         image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjM2Nzg4MzkwOF5BMl5BanBnXkFtZTgwNzA0OTE3NjE@._V1_UX182_CR0,0,182,268_AL_.jpg',
         imdbUrl:'http://www.imdb.com/title/tt2379713/'
     }
+
+
+    remove() {
+    alert('Eliminar');
+    }
 }
